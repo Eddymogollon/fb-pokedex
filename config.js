d9087bd911642e6c4be6d623911e3046a67e07d8
Eddy Mogollon <eddymogollon1@gmail.com>
2017-02-05 20:05:48 -0500

    improved it

---
+++ config.js
@@ -0,0 +1,7 @@
+const config = {
+  recast: '68792594d14026f2031d7906e5322ac5',
+  appId: '84ca3d50-6d39-4e99-8f31-1e2a56d107f4',
+  appPassword: 'Y0bJbU9iJ1cOyCJGVq7wzGo',
+};
+
+module.exports = config;
